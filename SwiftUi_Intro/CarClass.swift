a044305404a008a84b000bfd8b0bb6474a3db28b
shaxobiddin <shaxobiddins00@gmail.com>
2024-01-26 11:42:03 +0500

    simple UI

---
+++ SwiftUi_Intro/CarClass.swift
@@ -0,0 +1,8 @@
+//
+//  CarClass.swift
+//  SwiftUi_Intro
+//
+//  Created by Shaxobiddin on 26/01/24.
+//
+
+import Foundation
---
+++ SwiftUi_Intro/SwiftUi_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_IntroApp.swift
+//  SwiftUi_Intro
+//
+//  Created by Shaxobiddin on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
