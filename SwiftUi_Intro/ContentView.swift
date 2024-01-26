59fd56ed459f7b905c55d4549485397017ad60ea
shaxobiddin <shaxobiddins00@gmail.com>
2024-01-26 11:42:03 +0500

    Initial Commit

---
+++ SwiftUi_Intro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUi_Intro
+//
+//  Created by Shaxobiddin on 26/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
